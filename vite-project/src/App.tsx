b31c15de508957b20d0b11fa7dfd2c2d8658a56f
Tomáš <tomasolsiak1@gmail.com>
2023-12-21 10:52:03 +0100

    add error page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,6 +5,7 @@ import SharedLayout from "./Pages/SharedLayout";
 import AboutMe from "./Pages/AboutMe";
 import Projects from "./Pages/Projects";
 import ContactMe from "./Pages/ContactMe";
+import Error from "./Pages/Error";
 
 const App = () => {
   return (
@@ -17,7 +18,7 @@ const App = () => {
               <Route path="about-me" element={<AboutMe />}></Route>
               <Route path="projects" element={<Projects />}></Route>
               <Route path="/contact-me" element={<ContactMe />}></Route>
-              {/* <Route path="*" element={<Error />} /> */}
+              <Route path="*" element={<Error />} />
             </Route>
           </Routes>
         </MenuProvider>
---
+++ vite-project/src/Pages/Error.tsx
@@ -0,0 +1,27 @@
+import { Box, Typography } from "@mui/material";
+
+const Eroor = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h1" color="#607b96" fontSize={35}>
+          404 Not Found
+        </Typography>
+        <Typography variant="h4" color="#607b96" fontSize={20} mt={3}>
+          The page that does not exist!
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+export default Eroor;

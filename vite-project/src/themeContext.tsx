783fbb3b6a1ad0c51852f83dc34cf1b51a58d53a
Tomáš <tomasolsiak1@gmail.com>
2024-02-05 19:56:09 +0100

    update typescript datatypes

--- vite-project/src/themeContext.tsx
+++ vite-project/src/themeContext.tsx
@@ -1,11 +1,11 @@
-import { createContext, useContext, useState, useMemo } from 'react';
-import { CssBaseline } from '@mui/material';
+import { createContext, useContext, useState, useMemo } from "react";
+import { CssBaseline } from "@mui/material";
 import {
   createTheme,
   ThemeProvider as MUIThemeProvider,
-} from '@mui/material/styles';
-import { themeOptions } from './palete';
-import { themeOptionsDark } from './paleteDark';
+} from "@mui/material/styles";
+import { themeOptions } from "./palete";
+import { themeOptionsDark } from "./paleteDark";
 
 interface ThemeContextType {
   themeMode: string;
@@ -18,27 +18,31 @@ export const useThemeContext = () => {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error(
-      'useThemeContext must be used within a CustomThemeProvider',
+      "useThemeContext must be used within a CustomThemeProvider"
     );
   }
   return context;
 };
 
-export const CustomThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [themeMode, setThemeMode] = useState(
-    localStorage.getItem('themeMode') || 'light',
+export const CustomThemeProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [themeMode, setThemeMode] = useState<string>(
+    localStorage.getItem("themeMode") || "light"
   );
 
   const toggleTheme = () => {
     setThemeMode((prevMode) => {
-      const newMode = prevMode === 'light' ? 'dark' : 'light';
-      localStorage.setItem('themeMode', newMode);
+      const newMode = prevMode === "light" ? "dark" : "light";
+      localStorage.setItem("themeMode", newMode);
       return newMode;
     });
   };
 
   const theme = useMemo(() => {
-    return createTheme(themeMode === 'dark' ? themeOptions : themeOptionsDark);
+    return createTheme(themeMode === "dark" ? themeOptions : themeOptionsDark);
   }, [themeMode]);
 
   return (

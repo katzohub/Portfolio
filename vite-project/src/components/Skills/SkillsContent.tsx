b48c6a9dc4a44735b259600acb2c7ca85b1f560b
katzohub <tomasolsiak1@gmail.com>
2024-07-22 19:01:27 +0200

    create animate skills page

--- vite-project/src/components/Skills/SkillsContent.tsx
+++ vite-project/src/components/Skills/SkillsContent.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import dataSkills from "../../constants/DataSkills";
 import { CheckedStateProps } from "../../types";
 import {
@@ -13,6 +14,9 @@ import {
   StyledSkillCardBtn,
 } from "./StyledSkills";
 import { useIntl } from "react-intl";
+import { motion, AnimatePresence } from "framer-motion";
+
+const MotionStyledSkillsContainer = motion(StyledSkillsContainer);
 
 const SkillsContent = ({
   checkedState,
@@ -27,41 +31,58 @@ const SkillsContent = ({
           ([key, value]) => value && project.technologies.includes(key)
         )
       );
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setIsReady(true);
+  }, []);
 
   return (
-    <StyledSkillsContainer>
-      <StyledSkillsWrapp>
-        <StyledSkillsWrapper>
-          <StyledSkillsGridContainer>
-            {filteredProjects.map((project) => (
-              <StyledSkillsGridWrapper item key={project.id}>
-                <StyledSkillsCard>
-                  <img src={project.img} alt={project.nameProject} />
-                  <StyledSkillCardHeading>
-                    <h2>{intl.formatMessage({ id: project.nameProject })}</h2>
-                    <p>{intl.formatMessage({ id: project.detailProject })}</p>
+    <AnimatePresence>
+      {isReady && (
+        <MotionStyledSkillsContainer
+          initial={{ x: "100%" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <StyledSkillsWrapp>
+            <StyledSkillsWrapper>
+              <StyledSkillsGridContainer>
+                {filteredProjects.map((project) => (
+                  <StyledSkillsGridWrapper item key={project.id}>
+                    <StyledSkillsCard>
+                      <img src={project.img} alt={project.nameProject} />
+                      <StyledSkillCardHeading>
+                        <h2>
+                          {intl.formatMessage({ id: project.nameProject })}
+                        </h2>
+                        <p>
+                          {intl.formatMessage({ id: project.detailProject })}
+                        </p>
 
-                    <StyledSkillCardContainer>
-                      {project.icon &&
-                        project.icon.map((icon, index) => (
-                          <StyledSkillCardIconWrap key={index}>
-                            {icon}
-                          </StyledSkillCardIconWrap>
-                        ))}
-                    </StyledSkillCardContainer>
-                    <StyledSkillCardBtn variant="contained">
-                      <a href={project.openProject} target="_blank">
-                        {intl.formatMessage({ id: "project.viewProject" })}
-                      </a>
-                    </StyledSkillCardBtn>
-                  </StyledSkillCardHeading>
-                </StyledSkillsCard>
-              </StyledSkillsGridWrapper>
-            ))}
-          </StyledSkillsGridContainer>
-        </StyledSkillsWrapper>
-      </StyledSkillsWrapp>
-    </StyledSkillsContainer>
+                        <StyledSkillCardContainer>
+                          {project.icon &&
+                            project.icon.map((icon, index) => (
+                              <StyledSkillCardIconWrap key={index}>
+                                {icon}
+                              </StyledSkillCardIconWrap>
+                            ))}
+                        </StyledSkillCardContainer>
+                        <StyledSkillCardBtn variant="contained">
+                          <a href={project.openProject} target="_blank">
+                            {intl.formatMessage({ id: "project.viewProject" })}
+                          </a>
+                        </StyledSkillCardBtn>
+                      </StyledSkillCardHeading>
+                    </StyledSkillsCard>
+                  </StyledSkillsGridWrapper>
+                ))}
+              </StyledSkillsGridContainer>
+            </StyledSkillsWrapper>
+          </StyledSkillsWrapp>
+        </MotionStyledSkillsContainer>
+      )}
+    </AnimatePresence>
   );
 };
 
